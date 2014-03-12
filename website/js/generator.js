ff8d97b262d539c275513856861dec2f96c980bf
Guiks2 <guillaume.delapre@gmail.com>
2014-03-12 15:38:35 +0100

    Ajout canevas formulaire inscription
    Passage des pages en PHP
    Insertion du formulaire d'inscription dans l'index, il n'est pas encore
    relié à la BDD, des modifications seront à voir/faire sur la table user
    4è fois que je commit ces f*cking changements, espérons que ça soit la
    bonne.

---
+++ website/js/generator.js
@@ -0,0 +1,71 @@
+
+this.html = this.html || {};
+
+
+
+
+// Menu navigation
+html.Menu = function(args) {
+	this.cls = "generated-menu";
+	this.renderTo = args.render_to;
+	this.elements = args.elements || {};
+}
+
+html.Menu.prototype.init = function() {
+	var me = this;
+	me.container = $('<ul />');
+	me.container.addClass(me.cls);
+
+	$(this.elements).each(function(index, item) {
+		var li = $("<li/>");
+		li.text(item.label);
+
+		if(item.click)
+			$(li).on("click", function(e) {
+				item.click(e);
+			});
+		
+		me.container.append(li);
+	});
+
+	me.renderTo.append(me.container);
+};
+
+
+
+/*
+// Input type téléphone
+html.PhoneNumberInput = function(placeholder,error_check_callback, valid_check_callback, check_callback) {
+	this.cls = "myPhoneNumberInput";
+	this.type = "text";
+	this.placeholder = placeholder || "";
+	this.error_check_callback = error_check_callback;
+	this.valid_check_callback = valid_check_callback;
+	this.check_callback = check_callback;
+	this.regex = /(\+\d+(\s|-))?0\d(\s|-)?(\d{2}(\s|-)?){4}/;
+}
+
+// Héritage de phoneNumber
+html.PhoneNumberInput.prototype.setRenderTo = html.TextInput.prototype.setRenderTo;
+html.PhoneNumberInput.prototype.init = html.TextInput.prototype.init;
+html.PhoneNumberInput.prototype.keyListener = html.TextInput.prototype.keyListener;
+html.PhoneNumberInput.prototype.getValue = html.TextInput.prototype.getValue;
+
+html.PhoneNumberInput.prototype.check = function(e) {
+	var me = this;
+
+	if(me.element) {
+		if(me.element.val().match(me.regex)) {
+			if(!me.check_callback || me.check_callback(e, me.element)) {	
+				if(me.valid_check_callback)
+					me.valid_check_callback(e, me.element);
+				return true;
+			}
+		}
+	}
+	if(me.error_check_callback)
+		me.error_check_callback(e, me.element);
+
+	return false;
+}
+*/
