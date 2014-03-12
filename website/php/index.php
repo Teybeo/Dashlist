6b095230757709842e5c806b6d130084e61864ea
Guiks2 <guillaume.delapre@gmail.com>
2014-03-12 16:52:57 +0100

    Redirection correct des dossiers
    Le js et le css sont correctement pris en compte lors de l'appel dans
    index

--- website/php/index.php
+++ website/php/index.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Dashlist</title>
-	<link rel="stylesheet" href="css/style.css" type="text/css">
+	<link rel="stylesheet" href="../css/style.css" type="text/css">
     <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
 	<script src="../js/generator.js"></script>
     
