688c4fc2de493e53772e8ca3b38015d784f744e1
Thibault D'Archivio <thibault.darchivio@free.fr>
2014-03-21 23:03:27 +0100

    Correction et clarification des annulation
    Les events de suppression sont dans le bon

--- source/GUI/EventLog.java
+++ source/GUI/EventLog.java
@@ -17,7 +17,7 @@ public class EventLog {
 	ArrayList<Event> events;
 	private EventMenu event_menu;
 	private int board_id;
-	private static final int FIRST = 1;
+	private static final int FIRST = 0;
 	private static final int LAST = -1;
 
 	public EventLog(int board_id) {
@@ -34,6 +34,8 @@ public class EventLog {
 		EventDAO dao = new EventDAO(BddConnection.getInstance());
 		events = dao.getEventsByBoard(board_id);
 
+		// Les events sont triés du plus récent au plus vieux
+		// donc on les insère chacun après tous les autres
 		for (Event event : events)
 			setup_event(event, LAST);
 
@@ -47,6 +49,7 @@ public class EventLog {
 		label.setName(String.valueOf(event.getId()));
 		label.addMouseListener(event_menu.getLabelListener());
 		System.out.println(event.toString());
+
 		log_zone.add(label, position);
 	}
 
@@ -58,9 +61,9 @@ public class EventLog {
 		EventDAO dao = new EventDAO(BddConnection.getInstance());
         ArrayList<Event> new_events;
 
-		// On ne récupère que les events générés après le dernier de ceux qu'on a déjà
-		if (events.size() >= 1)
-			new_events = dao.getEventsByBoardAfter(board_id, events.get(1).getDate());
+		// On ne récupère que les events générés après le plus récent (premier) de ceux qu'on à déjà
+		if (events.size() > 0)
+			new_events = dao.getEventsByBoardAfter(board_id, events.get(0).getDate());
 		else
 			new_events = dao.getEventsByBoard(board_id); // Si on en avait pas, on prend tout
 
@@ -72,8 +75,8 @@ public class EventLog {
 		}
 
 		// Et on les insère en premier dans la mémoire
-		for (Event new_ev : new_events)
-			events.add(1, new_ev);
+		new_events.addAll(events);
+		events = new_events;
 
 		scroll_pane.getParent().revalidate();
 		scroll_pane.getParent().repaint();
@@ -87,29 +90,52 @@ public class EventLog {
 
 	private void revertTo(Event event) {
 
-		Iterator<Event> iterator = events.iterator();
-		Event ev_tmp = null;
 		EventDAO dao = new EventDAO(BddConnection.getInstance());
 
-		while (iterator.hasNext())
-		{
-			ev_tmp = iterator.next();
-			System.out.println("Annulation de "+ev_tmp.getReadableDescription());
+		ArrayList<Event> following_events = findFollowingEvents(event);
 
-			if (dao.revert(ev_tmp) == true)
+		for (Event event_to_revert : following_events) {
+
+			System.out.println("Annulation de "+event_to_revert.getReadableDescription());
+
+			// Temporaire, revert() n'est pas implémentée pour tous les types d'events
+			// Si un type n'est pas implémentée, elle renvoie false et ne change rien à la base
+			// Dans ces cas-la, on ne fait rien non plus dans l'appli sur ces events
+			if (dao.revert(event_to_revert) == true)
 			{
-				events.remove(ev_tmp);
-				log_zone.remove(findLabel(ev_tmp.getId()));
+				events.remove(event_to_revert);
+				log_zone.remove(findLabel(event_to_revert.getId()));
 			}
-
-			// Une fois atteint l'event ciblé, on s'arrête
-			if (ev_tmp.getId() == event.getId())
-				break;
 		}
 
 		scroll_pane.getParent().revalidate();
 		scroll_pane.getParent().repaint();
 
+	}
+
+	/**
+	 * Récupère la liste de tous les events du journal actuel qui se sont déroulés après l'event donné en paramètre
+	 * @param event L'event auquel sont postérieurs les events renvoyés par la fonction
+	 * @return La liste des events de la board actuelle postérieurs à event. Liste vide si pas d'events postérieurs
+	 */
+	private ArrayList<Event> findFollowingEvents(Event event) {
+
+		ArrayList<Event> following_events = new ArrayList<>();
+
+		// Les events sont triés du plus récent au plus vieux
+		for (Event event_tmp : events)
+		{
+			// Une fois atteint l'event ciblé, on s'arrête
+			if (event_tmp.getId() == event.getId())
+				break;
+
+			// Tant qu'on a pas trouvé l'event, on ajoute à la liste
+			// Vérification au cas où, le bon tri de la liste est sensé être assuré
+			if (event_tmp.getDate().after(event.getDate()))
+				following_events.add(event_tmp);
+		}
+
+		return following_events;
 	}
 
 	private JLabel findLabel(int id) {
