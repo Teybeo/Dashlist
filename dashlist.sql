9c0510475eb40b31b0171fb21e1a4b962d15243d
Thibault D'Archivio <thibault.darchivio@free.fr>
2014-04-23 12:00:02 +0200

    Maj de la BDD

--- dashlist.sql
+++ dashlist.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client: localhost
--- Généré le : Dim 16 Mars 2014 à 13:23
+-- Généré le : Mer 23 Avril 2014 à 09:58
 -- Version du serveur: 5.5.16
 -- Version de PHP: 5.3.8
 
@@ -30,19 +30,7 @@ CREATE TABLE IF NOT EXISTS `board` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(40) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=26 ;
-
---
--- Contenu de la table `board`
---
-
-INSERT INTO `board` (`id`, `name`) VALUES
-(1, 'devoirs'),
-(2, 'vacances'),
-(3, 'courses'),
-(4, 'travaux'),
-(5, 'bugs'),
-(6, 'test');
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
 
 -- --------------------------------------------------------
 
@@ -59,18 +47,6 @@ CREATE TABLE IF NOT EXISTS `board_members` (
   KEY `user_id` (`user_id`),
   KEY `board_id_2` (`board_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Contenu de la table `board_members`
---
-
-INSERT INTO `board_members` (`board_id`, `user_id`, `is_admin`, `is_pending`) VALUES
-(1, 1, 1, 0),
-(2, 1, 0, 0),
-(3, 2, 0, 0),
-(4, 3, 0, 0),
-(5, 1, 0, 0),
-(6, 3, 1, 0);
 
 -- --------------------------------------------------------
 
@@ -109,7 +85,7 @@ CREATE TABLE IF NOT EXISTS `event` (
   KEY `item_id_new` (`item_id_new`),
   KEY `list_id_old` (`list_id_old`),
   KEY `user_id` (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=24 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=104 ;
 
 -- --------------------------------------------------------
 
@@ -124,21 +100,11 @@ CREATE TABLE IF NOT EXISTS `item` (
   `date` date NOT NULL,
   `description` text,
   `position` int(11) NOT NULL,
+  `is_deleted` tinyint(1) NOT NULL,
   `cover` varchar(512) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `id_list` (`id_list`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=76 ;
-
---
--- Contenu de la table `item`
---
-
-INSERT INTO `item` (`id`, `id_list`, `name`, `date`, `description`, `position`, `cover`) VALUES
-(3, 1, 'Exo 10 et 11 page 45', '2014-02-25', '', 1, ''),
-(4, 1, 'DM trigo', '2014-02-28', 'Exo 48 page 75', 2, ''),
-(5, 1, 'Exo 48', '2014-03-02', NULL, 3, NULL),
-(21, 2, 'Contrôle 24/01', '2014-03-02', NULL, 1, NULL),
-(22, 27, 'Dissert 28/01', '2014-03-02', NULL, 1, NULL);
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=107 ;
 
 -- --------------------------------------------------------
 
@@ -153,25 +119,7 @@ CREATE TABLE IF NOT EXISTS `list` (
   `position` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `id_board` (`id_board`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=86 ;
-
---
--- Contenu de la table `list`
---
-
-INSERT INTO `list` (`id`, `id_board`, `name`, `position`) VALUES
-(1, 1, 'Maths', 1),
-(2, 1, 'SVT', 2),
-(5, 2, 'Transport', 1),
-(6, 2, 'Finances', 2),
-(7, 2, 'Choses à faires', 3),
-(10, 3, 'Bouffe', 1),
-(11, 3, 'Autre', 2),
-(12, 4, 'Salle de bains', 1),
-(13, 4, 'Grenier', 2),
-(14, 4, 'Cuisine', 3),
-(15, 4, 'Peinture', 4),
-(27, 1, 'Philo', 3);
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=99 ;
 
 -- --------------------------------------------------------
 
