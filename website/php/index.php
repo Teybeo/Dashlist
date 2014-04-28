33a1aedd5f1efdd04ff06c899e031abad0e37677
ThibaultCrosnier <thibault.crosnier@gmail.com>
2014-04-29 00:49:00 +0200

    Page d'accueil
    + Fix mineurs d'input text
    Mention spéciale au bleu et vert sur la même page.

--- website/php/index.php
+++ website/php/index.php
@@ -5,26 +5,29 @@
 	<div id="contents">
 		<div id="tagline" class="clearfix">
 			<h1>Gérez vos projets simplement avec <b>Dashlist</b></h1>
+			<img style="border: solid 3px #555555" src="../images/screen.png"/><br/><br/>
 			<div>
 				<p>
-					+ Capture écran Dashlist
+					<b>Qu'est ce que Dashlist ?</b>
 				</p>
 				<p>
-					Non, vraiment, ça apporterait un cachet
+					Dashlist est une application de gestion simplifiée de projet. <br/>
+					En d'autres mots, Dashlist vous permet de créer à votre guise des tâches pour organiser le plus efficacement possible vos priorités.<br/>
+					Vous pouvez ainsi créer un tableau personnel, ou au contraire le partager avec vos contacts qui pourront également intéragir avec celui-ci.
 				</p>
-				<p>
-					Ca ferait genre logiciel pro et tout, qu'on rigole pas dans le métier quoi. 
-				</p>
+				
 			</div>
 			<div>
 				<p>
-					Présentation rapide de Dashlist
+					<b>Comment est né Dashlist ?</b>
 				</p>
 				<p>
-					Explication des fonctionnalités de base et les trucs classes que tu peux faire avec
+					Dashlist est le projet annuel de 3 étudiants de l'ESGI en 3ème année de la filière "Architecture Logicielle"<br/>
+						Thibault CROSNIER<br/>
+						Thibault D'ARCHIVIO<br/>
+						Guillaume DELAPRE
 				</p>
-				<p>
-					Après t'improvises, tu cherches ce qui est le mieux à mettre pour rendre le site bien 	
+				<p> 	
 				</p>
 			</div>
 		</div>
